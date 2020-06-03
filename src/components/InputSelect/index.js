0fab4e45f420b4e8ee23fd923ec36487906ba104
Michelon Souza <michelon.souza@hotmail.com>
2020-06-03 00:57:47 -0300

    inital commit

---
+++ src/components/InputSelect/index.js
@@ -0,0 +1,49 @@
+import React, { useEffect, useState, useCallback } from 'react';
+import SVG from 'react-inlinesvg';
+
+import iconMarker from '../../assets/images/icon-marker.svg';
+import logo from '../../assets/images/webmotors_logo.svg';
+import getLocation from '../../config/getLocation';
+
+import { Container } from './styles';
+
+function InputSelect() {
+  const [locale, setLocale] = useState('');
+  const [loading, setLoading] = useState(true);
+
+  const setLocation = useCallback(location => {
+    if (location) {
+      setLocale(location);
+    }
+
+    setLoading(false);
+  }, []);
+
+  useEffect(() => {
+    getLocation(setLocation);
+  }, [setLocation]);
+
+  return (
+    <Container>
+      <div className="input-container">
+        <input
+          type="text"
+          value={locale}
+          onChange={e => setLocale(e.target.value)}
+          placeholder="Insira sua cidade e estado"
+        />
+        <SVG src={iconMarker} />
+        {loading && <img src={logo} alt="WebMotors" />}
+      </div>
+
+      <select name="" id="" className="distance-select">
+        <option value="50">Raio: 50km</option>
+        <option value="100">Raio: 100km</option>
+        <option value="150">Raio: 150km</option>
+        <option value="200">Raio: 200km</option>
+      </select>
+    </Container>
+  );
+}
+
+export default InputSelect;
